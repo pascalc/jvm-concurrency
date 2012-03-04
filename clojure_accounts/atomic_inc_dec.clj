ff068093e9cb561fcf38cd1ab18d89a916288b72
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2012-03-04 16:10:35 +0100

    Added an example with Clojure STM

---
+++ clojure_accounts/atomic_inc_dec.clj
@@ -0,0 +1,25 @@
+(defn insert [balance amount]
+    (dosync
+        (println "Going to insert" amount)
+        (let [current-balance @balance]
+            (. Thread sleep 100)
+            (alter balance + amount)
+            (println "Inserted" amount))))
+
+(defn withdraw [balance amount]
+    (dosync
+        (println "Going to withdraw" amount)
+        (let [current-balance @balance]
+            (. Thread sleep 100)
+            (alter balance - amount)
+            (println "Withdrew" amount))))
+
+(def balance (ref 0))
+(println "Balance is" @balance)
+
+(future (insert balance 1))
+(future (withdraw balance 1))
+
+(. Thread sleep 1000)
+
+(println "Finishing balance:" @balance)
